674eb6f7acc404aa5f4b0cab8103d86c33fba52e
Stanley Ko <influenz@daum.net>
2015-10-12 00:19:05 +0900

    base code

---
+++ picnicapp/src/com/kou/picnicapp/KalmanFilter.java
@@ -0,0 +1,35 @@
+package com.kou.picnicapp;
+
+public class KalmanFilter {
+
+	// Extended Kalmna Filter가 아님. 그냥 칼만필터.
+
+	private double Q = 0.000091;
+	private double R = 0.0076;
+
+	private double X = 0, P = 1, K;
+
+	public KalmanFilter(float initValue) {
+		X = initValue;
+	}
+
+	private void measurementUpdate() {
+		K = (P + Q) / (P + Q + R);
+		P = R * (P + Q) / (R + P + Q);
+	}
+
+	public double update(double measurement) {
+		measurementUpdate();
+		X = X + (measurement - X) * K;
+
+		return X;
+	}
+
+	public void init(float initValue) {
+		X = initValue;
+	}
+
+	public double getData() {
+		return X;
+	}
+}
