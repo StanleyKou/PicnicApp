a5b54797c4090b90dac9a66bc6a22d0518dcd88c
redcrusader <influenz@daum.net>
2015-10-19 01:12:01 +0900

    use excel lib

--- picnicapp/src/com/kou/picnicapp/DeviceScanActivity.java
+++ picnicapp/src/com/kou/picnicapp/DeviceScanActivity.java
@@ -41,7 +41,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kou.picnicapp.base.BaseActivity;
-import com.kou.picnicapp.model.TargetItem;
+import com.kou.picnicapp.model.TargetData;
 import com.kou.picnicapp.utils.LogWrapper;
 
 public class DeviceScanActivity extends BaseActivity implements OnClickListener {
@@ -57,9 +57,7 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 	private static final long SCAN_PERIOD = 60000;
 
 	private ListView lvDevice;
-	private listAdapter listAdapter;
-
-	private TextView tvBeaconScanNodata;
+	private TargetListAdapter listAdapter;
 
 	private Button btnScanStart;
 	private Button btnScanStop;
@@ -92,11 +90,9 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 		lvDevice = (ListView) findViewById(R.id.lvDevice);
 		lvDevice.setDivider(null);
 
-		listAdapter = new listAdapter();
+		listAdapter = new TargetListAdapter();
 		lvDevice.setAdapter(listAdapter);
 		lvDevice.setOnItemClickListener(onItemClickListener);
-
-		tvBeaconScanNodata = (TextView) findViewById(R.id.tvBeaconScanNodata);
 
 		btnScanStart = (Button) findViewById(R.id.btnScanStart);
 		btnScanStop = (Button) findViewById(R.id.btnScanStop);
@@ -137,9 +133,6 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 	// }
 
 	private void performStartScan() {
-		listAdapter.clear();
-		tvBeaconScanNodata.setVisibility(View.VISIBLE);
-		lvDevice.setVisibility(View.GONE);
 		scanLeDevice(true);
 	}
 
@@ -166,6 +159,9 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 		}
 
 		scanLeDevice(true);
+
+		setDummyData();
+
 	}
 
 	@Override
@@ -182,16 +178,16 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 	protected void onPause() {
 		super.onPause();
 		scanLeDevice(false);
-		listAdapter.clear();
+
 	}
 
 	private OnItemClickListener onItemClickListener = new OnItemClickListener() {
 
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			final BluetoothDevice device = listAdapter.getDevice(position);
-			if (device == null)
-				return;
+			// final BluetoothDevice device = listAdapter.getDevice(position);
+			// if (device == null)
+			// return;
 			// final Intent intent = new Intent(this, DeviceControlActivity.class);
 			// intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
 			// intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
@@ -243,41 +239,42 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 	}
 
 	// Adapter for holding devices found through scanning.
-	private class listAdapter extends BaseAdapter {
-		private ArrayList<TargetItem> leTargetItems;
+	private class TargetListAdapter extends BaseAdapter {
+		private ArrayList<TargetData> targetlist = new ArrayList<TargetData>();
 		private LayoutInflater inflator;
 
-		public listAdapter() {
+		public TargetListAdapter() {
 			super();
-			leTargetItems = new ArrayList<TargetItem>();
 			inflator = DeviceScanActivity.this.getLayoutInflater();
 		}
 
 		public void checkTarget(BluetoothDevice device) {
-			if (!leTargetItems.contains(device)) {
-				// leDevices.add(device);
-
-				tvBeaconScanNodata.setVisibility(View.GONE);
-				lvDevice.setVisibility(View.VISIBLE);
-			}
+			// if (!leTargetItems.contains(device)) {
+			// // leDevices.add(device);
+			//
+			// }
 		}
 
-		public TargetItem getDevice(int position) {
-			return leTargetItems.get(position);
+		public void setTargetData(ArrayList<TargetData> targetlist) {
+			this.targetlist = targetlist;
+		}
+
+		public TargetData getDevice(int position) {
+			return targetlist.get(position);
 		}
 
 		public void clear() {
-			leTargetItems.clear();
+			targetlist.clear();
 		}
 
 		@Override
 		public int getCount() {
-			return leTargetItems.size();
+			return targetlist.size();
 		}
 
 		@Override
 		public Object getItem(int i) {
-			return leTargetItems.get(i);
+			return targetlist.get(i);
 		}
 
 		@Override
@@ -290,16 +287,23 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 			ViewHolder viewHolder;
 			// General ListView optimization code.
 			if (view == null) {
-				view = inflator.inflate(R.layout.listitem_device, null);
+				view = inflator.inflate(R.layout.listitem_target, null);
 				viewHolder = new ViewHolder();
-				viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
-				viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+				viewHolder.tvTargetNumber = (TextView) view.findViewById(R.id.tvTargetNumber);
+				viewHolder.tvTargetName = (TextView) view.findViewById(R.id.tvTargetName);
+				viewHolder.tvTargetPhone = (TextView) view.findViewById(R.id.tvTargetPhone);
 				view.setTag(viewHolder);
 			} else {
 				viewHolder = (ViewHolder) view.getTag();
 			}
 
 			// FIXME: Display
+
+			TargetData target = targetlist.get(i);
+
+			viewHolder.tvTargetNumber.setText(target.number + "");
+			viewHolder.tvTargetName.setText(target.name);
+			viewHolder.tvTargetPhone.setText(target.phoneNumber);
 
 			// BluetoothDevice device = leTargetItems.get(i);
 			// final String deviceName = device.getName();
@@ -311,6 +315,12 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 
 			return view;
 		}
+	}
+
+	static class ViewHolder {
+		TextView tvTargetNumber;
+		TextView tvTargetName;
+		TextView tvTargetPhone;
 	}
 
 	HashMap<String, KalmanFilter> filteredList = new HashMap<String, KalmanFilter>();
@@ -342,9 +352,150 @@ public class DeviceScanActivity extends BaseActivity implements OnClickListener 
 		}
 	};
 
-	static class ViewHolder {
-		TextView deviceName;
-		TextView deviceAddress;
+	private void setDummyData() {
+		ArrayList<TargetData> dummyTargetlist = new ArrayList<TargetData>();
+
+		TargetData target1 = new TargetData();
+		TargetData target2 = new TargetData();
+		TargetData target3 = new TargetData();
+		TargetData target4 = new TargetData();
+		TargetData target5 = new TargetData();
+		TargetData target6 = new TargetData();
+		TargetData target7 = new TargetData();
+		TargetData target8 = new TargetData();
+		TargetData target9 = new TargetData();
+		TargetData target10 = new TargetData();
+
+		target1.sequence_id = 1;
+		target2.sequence_id = 2;
+		target3.sequence_id = 3;
+		target4.sequence_id = 4;
+		target5.sequence_id = 5;
+		target6.sequence_id = 6;
+		target7.sequence_id = 7;
+		target8.sequence_id = 8;
+		target9.sequence_id = 9;
+		target10.sequence_id = 10;
+
+		target1.number = "1";
+		target2.number = "2";
+		target3.number = "3";
+		target4.number = "4";
+		target5.number = "5";
+		target6.number = "6";
+		target7.number = "7";
+		target8.number = "8";
+		target9.number = "9";
+		target10.number = "10";
+
+		target1.name = "김일동";
+		target2.name = "김이동";
+		target3.name = "김삼동";
+		target4.name = "김사동";
+		target5.name = "김오동";
+		target6.name = "김육동";
+		target7.name = "김칠동";
+		target8.name = "김팔동";
+		target9.name = "김구동";
+		target10.name = "김십동";
+
+		target1.phoneNumber = "010-1111-0001";
+		target2.phoneNumber = "010-1111-0002";
+		target3.phoneNumber = "010-1111-0003";
+		target4.phoneNumber = "010-1111-0004";
+		target5.phoneNumber = "010-1111-0005";
+		target6.phoneNumber = "010-1111-0006";
+		target7.phoneNumber = "010-1111-0007";
+		target8.phoneNumber = "010-1111-0008";
+		target9.phoneNumber = "010-1111-0009";
+		target10.phoneNumber = "010-1111-0010";
+
+		target1.uuid = "1234";
+		target2.uuid = "1234";
+		target3.uuid = "1234";
+		target4.uuid = "1234";
+		target5.uuid = "1234";
+		target6.uuid = "1234";
+		target7.uuid = "1234";
+		target8.uuid = "1234";
+		target9.uuid = "1234";
+		target10.uuid = "1234";
+
+		target1.major = "16";
+		target2.major = "16";
+		target3.major = "16";
+		target4.major = "16";
+		target5.major = "16";
+		target6.major = "16";
+		target7.major = "16";
+		target8.major = "16";
+		target9.major = "16";
+		target10.major = "16";
+
+		target1.minor = "10002";
+		target2.minor = "16";
+		target3.minor = "16";
+		target4.minor = "16";
+		target5.minor = "16";
+		target6.minor = "16";
+		target7.minor = "16";
+		target8.minor = "16";
+		target9.minor = "16";
+		target10.minor = "16";
+
+		dummyTargetlist.add(target1);
+		dummyTargetlist.add(target2);
+		dummyTargetlist.add(target3);
+		dummyTargetlist.add(target4);
+		dummyTargetlist.add(target5);
+		dummyTargetlist.add(target6);
+		dummyTargetlist.add(target7);
+		dummyTargetlist.add(target8);
+		dummyTargetlist.add(target9);
+		dummyTargetlist.add(target10);
+
+		listAdapter.setTargetData(dummyTargetlist);
+
 	}
+
+	// <!-- http://www.andykhan.com/jexcelapi/index.html LGPL-->
+	// Workbook workbook = null;
+	// Sheet sheet = null;
+	//
+	// try {
+	// InputStream is = getBaseContext().getResources().getAssets().open("notes.xlsx");
+	// workbook = Workbook.getWorkbook(is);
+	//
+	// if (workbook != null) {
+	// sheet = workbook.getSheet(0);
+	//
+	// if (sheet != null) {
+	//
+	// int nMaxColumn = 2;
+	// int nRowStartIndex = 0;
+	// int nRowEndIndex = sheet.getColumn(nMaxColumn - 1).length - 1;
+	// int nColumnStartIndex = 0;
+	// int nColumnEndIndex = sheet.getRow(2).length - 1;
+	//
+	// dbAdapter.open();
+	// for (int nRow = nRowStartIndex; nRow <= nRowEndIndex; nRow++) {
+	// String title = sheet.getCell(nColumnStartIndex, nRow).getContents();
+	// String body = sheet.getCell(nColumnStartIndex + 1, nRow).getContents();
+	// dbAdapter.createNote(title, body);
+	// }
+	// dbAdapter.close();
+	// } else {
+	// System.out.println("Sheet is null!!");
+	// }
+	// } else {
+	// System.out.println("WorkBook is null!!");
+	// }
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// } finally {
+	// if (workbook != null) {
+	// workbook.close();
+	// }
+	// }
 
 }
---
+++ picnicapp/src/com/kou/picnicapp/model/TargetData.java
@@ -0,0 +1,14 @@
+package com.kou.picnicapp.model;
+
+public class TargetData {
+
+	public int sequence_id;
+	public String number;
+	public String name;
+	public long lastCheckedTime;
+	public String phoneNumber; // FIXME: Call to target, if he has phone number
+	public String uuid;
+	public String major;
+	public String minor;
+
+}
