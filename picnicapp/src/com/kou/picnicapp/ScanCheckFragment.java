090d09c47a8ad73db8b879e04e74d758a4739f48
Stanley Ko <influenz@daum.net>
2015-10-30 16:38:23 +0900

    - sort - long click phone call

--- picnicapp/src/com/kou/picnicapp/ScanCheckFragment.java
+++ picnicapp/src/com/kou/picnicapp/ScanCheckFragment.java
@@ -1,15 +1,22 @@
 package com.kou.picnicapp;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -19,6 +26,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -54,6 +62,10 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 	private int scanCount = 0;
 	private int checkCount = 0;
 	private Button btnScan;
+
+	private boolean isSortUse = false;
+	private Button btnSort;
+
 	private boolean isScanning;
 	private Handler handler = new Handler();
 
@@ -100,9 +112,12 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 		listAdapter = new TargetListAdapter();
 		lvDevice.setAdapter(listAdapter);
 		lvDevice.setOnItemClickListener(onItemClickListener);
+		lvDevice.setOnItemLongClickListener(onItemLongClickListener);
 
 		btnScan = (Button) mainView.findViewById(R.id.btnScan);
+		btnSort = (Button) mainView.findViewById(R.id.btnSort);
 		btnScan.setOnClickListener(this);
+		btnSort.setOnClickListener(this);
 
 		return mainView;
 	}
@@ -123,7 +138,7 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 	}
 
 	private void performStartScan() {
-
+		checkCount = 0;
 		scanCount = SCAN_COUNT_MAX + 1;
 		handler.post(countDownScanRunnable);
 		clearCheckState();
@@ -200,6 +215,21 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 		}
 	};
 
+	private OnItemLongClickListener onItemLongClickListener = new OnItemLongClickListener() {
+
+		@Override
+		public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+			TargetData t = listAdapter.getItem(position);
+			String phoneNum = t.getPhoneNumber();
+
+			Dialog d = createPhoneDialog(phoneNum);
+			d.show();
+
+			return false;
+		}
+	};
+
 	// Adapter for holding devices found through scanning.
 	private class TargetListAdapter extends BaseAdapter {
 		private ArrayList<TargetData> targetDatalist = new ArrayList<TargetData>();
@@ -233,6 +263,14 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 					}
 				}
 			}
+
+			if (isSortUse) {
+				sortNotFoundFirst();
+			} else {
+				// sortNumber();
+			}
+
+			listAdapter.notifyDataSetChanged();
 		}
 
 		public void setTargetData(ArrayList<TargetData> targetDatalist) {
@@ -253,13 +291,22 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 		}
 
 		@Override
-		public Object getItem(int i) {
+		public TargetData getItem(int i) {
 			return targetDatalist.get(i);
 		}
 
 		@Override
 		public long getItemId(int i) {
 			return i;
+		}
+
+		public void sortNumber() {
+			Collections.sort(targetDatalist, numComparator);
+		}
+
+		public void sortNotFoundFirst() {
+			Collections.sort(targetDatalist, numComparator);
+			Collections.sort(targetDatalist, foundComparator);
 		}
 
 		@Override
@@ -306,6 +353,38 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 		}
 	}
 
+	Comparator<TargetData> numComparator = new Comparator<TargetData>() {
+
+		@Override
+		public int compare(TargetData lhs, TargetData rhs) {
+
+			int lhsNum = Integer.parseInt(lhs.getNumber());
+			int rhsNum = Integer.parseInt(rhs.getNumber());
+
+			return (lhsNum > rhsNum) ? 1 : (lhsNum < rhsNum) ? -1 : 0;
+		}
+	};
+
+	Comparator<TargetData> foundComparator = new Comparator<TargetData>() {
+
+		@Override
+		public int compare(TargetData lhs, TargetData rhs) {
+
+			int lhsNum = 0;
+			int rhsNum = 0;
+
+			if (lhs.getCheckState() == CHECK_STATE.FOUND) {
+				lhsNum = 1;
+			}
+
+			if (rhs.getCheckState() == CHECK_STATE.FOUND) {
+				rhsNum = 1;
+			}
+
+			return (lhsNum > rhsNum) ? 1 : (lhsNum < rhsNum) ? -1 : 0;
+		}
+	};
+
 	static class ViewHolder {
 		RelativeLayout rlListItem;
 		RelativeLayout rlRange;
@@ -344,7 +423,6 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 				@Override
 				public void run() {
 					listAdapter.checkTarget(beacon);
-					listAdapter.notifyDataSetChanged();
 				}
 			});
 		}
@@ -375,6 +453,49 @@ public class ScanCheckFragment extends Fragment implements OnClickListener {
 			performStartScan();
 			break;
 
+		case R.id.btnSort:
+			if (isSortUse == true) {
+				isSortUse = false;
+				btnSort.setBackgroundResource(R.drawable.btn_sort_notselected);
+				listAdapter.sortNumber();
+			} else {
+				isSortUse = true;
+				btnSort.setBackgroundResource(R.drawable.btn_sort);
+				listAdapter.sortNotFoundFirst();
+			}
+
+			getActivity().runOnUiThread(new Runnable() {
+
+				@Override
+				public void run() {
+					listAdapter.notifyDataSetChanged();
+				}
+			});
+
+			break;
 		}
 	}
+
+	private String[] phoneNumberList;
+
+	protected Dialog createPhoneDialog(String number) {
+		Dialog dialog = null;
+
+		AlertDialog.Builder builder = new Builder(getActivity());
+		builder.setTitle(getString(R.string.send_call));
+		phoneNumberList = new String[] { number, };
+
+		builder.setItems(phoneNumberList, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				String chosenPhoneNumber = phoneNumberList[which];
+				Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + chosenPhoneNumber));
+				startActivity(intent);
+
+			}
+		});
+
+		dialog = builder.create();
+		return dialog;
+	}
+
 }
